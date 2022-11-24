8dbdaa79dfdcad8b7fa3d71ec46c538878c4525e
archan0621 <joshua_0621@naver.com>
2022-11-24 05:21:47 +0000

    edit ping timeout

--- ping.c
+++ ping.c
@@ -2,7 +2,7 @@
 int ping_main(char *address, int count){
 
     char buf[256];
-    sprintf(buf, "timeout 1 ping -c %d %s > /dev/null", count, address);
+    sprintf(buf, "timeout 4 ping -c %d %s > /dev/null", count, address);
     printf("IP : %s Count : %d\n", address, count);
     //if system(buf) result not return in 1second return 100
     int result = system(buf);
